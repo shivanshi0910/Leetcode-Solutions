1864fb04ac17a59e0154d24b8b7c7757cd38dece
shivanshi0910 <132770256+shivanshi0910@users.noreply.github.com>
2023-07-10 15:18:10 +0530

    Time: 103 ms (65.88%), Space: 59 MB (26.4%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -5,10 +5,8 @@ public:
         int i=0, j=n-1;
         int ans = INT_MIN;
         while(i < j) {
-            int minh = min(height[i] , height[j]);
-            ans = max(ans, (j-i)*minh);
-            while(height[i] <= minh && i<j) i++;
-            while(height[j] <= minh && i<j) j--;
+            ans = max( ans, (j-i)*min(height[i],height[j]) );
+            height[i] > height[j] ? j--:i++;
         }
         return ans;
     }
